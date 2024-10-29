b5f13518ae415a7e6e1de75e238fc387ae60e6cb
mayank <ukeym160@gmail.com>
2024-10-29 16:07:56 +0530

    Guess Number Game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,62 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+function displayMessage(message) {
+  document.querySelector('.message').textContent = message;
+}
+
+function displayScore(score) {
+  document.querySelector('.score').textContent = score;
+}
+
+function displayHighScore(highScore) {
+  document.querySelector('.highscore').textContent = highScore;
+}
+
+function displayNumber(number) {
+  document.querySelector('.number').textContent = number;
+}
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('Not Valid Input!');
+  } else if (guess === secretNumber) {
+    displayMessage('Correct Number!');
+    displayNumber(secretNumber);
+
+    if (score > highScore) {
+      highScore = score;
+      displayHighScore(highScore);
+    }
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? 'Too High!' : 'Too Low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('You lost the Game!');
+      displayScore(0);
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  displayMessage('Start guessing...');
+  displayScore(score);
+  displayNumber('?');
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
