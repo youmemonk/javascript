26fae1cb7b941ac9f33ee0055285035e361c30b8
mayank <ukeym160@gmail.com>
2024-11-08 01:41:47 +0530

    Data Structures

---
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -0,0 +1,387 @@
+// DESTRUCTURING ARRAYS
+const books = [
+  {
+    title: 'Algorithms',
+    author: ['Robert Sedgewick', 'Kevin Wayne'],
+    publisher: 'Addison-Wesley Professional',
+    publicationDate: '2011-03-24',
+    edition: 4,
+    keywords: [
+      'computer science',
+      'programming',
+      'algorithms',
+      'data structures',
+      'java',
+      'math',
+      'software',
+      'engineering',
+    ],
+    pages: 976,
+    format: 'hardcover',
+    ISBN: '9780321573513',
+    language: 'English',
+    programmingLanguage: 'Java',
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.41,
+        ratingsCount: 1733,
+        reviewsCount: 63,
+        fiveStarRatingCount: 976,
+        oneStarRatingCount: 13,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Structure and Interpretation of Computer Programs',
+    author: [
+      'Harold Abelson',
+      'Gerald Jay Sussman',
+      'Julie Sussman (Contributor)',
+    ],
+    publisher: 'The MIT Press',
+    publicationDate: '2022-04-12',
+    edition: 2,
+    keywords: [
+      'computer science',
+      'programming',
+      'javascript',
+      'software',
+      'engineering',
+    ],
+    pages: 640,
+    format: 'paperback',
+    ISBN: '9780262543231',
+    language: 'English',
+    programmingLanguage: 'JavaScript',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.36,
+        ratingsCount: 14,
+        reviewsCount: 3,
+        fiveStarRatingCount: 8,
+        oneStarRatingCount: 0,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: "Computer Systems: A Programmer's Perspective",
+    author: ['Randal E. Bryant', "David Richard O'Hallaron"],
+    publisher: 'Prentice Hall',
+    publicationDate: '2002-01-01',
+    edition: 1,
+    keywords: [
+      'computer science',
+      'computer systems',
+      'programming',
+      'software',
+      'C',
+      'engineering',
+    ],
+    pages: 978,
+    format: 'hardcover',
+    ISBN: '9780130340740',
+    language: 'English',
+    programmingLanguage: 'C',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 4.44,
+        ratingsCount: 1010,
+        reviewsCount: 57,
+        fiveStarRatingCount: 638,
+        oneStarRatingCount: 16,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'Operating System Concepts',
+    author: ['Abraham Silberschatz', 'Peter B. Galvin', 'Greg Gagne'],
+    publisher: 'John Wiley & Sons',
+    publicationDate: '2004-12-14',
+    edition: 10,
+    keywords: [
+      'computer science',
+      'operating systems',
+      'programming',
+      'software',
+      'C',
+      'Java',
+      'engineering',
+    ],
+    pages: 921,
+    format: 'hardcover',
+    ISBN: '9780471694663',
+    language: 'English',
+    programmingLanguage: 'C, Java',
+    onlineContent: false,
+    thirdParty: {
+      goodreads: {
+        rating: 3.9,
+        ratingsCount: 2131,
+        reviewsCount: 114,
+        fiveStarRatingCount: 728,
+        oneStarRatingCount: 65,
+      },
+    },
+  },
+  {
+    title: 'Engineering Mathematics',
+    author: ['K.A. Stroud', 'Dexter J. Booth'],
+    publisher: 'Palgrave',
+    publicationDate: '2007-01-01',
+    edition: 14,
+    keywords: ['mathematics', 'engineering'],
+    pages: 1288,
+    format: 'paperback',
+    ISBN: '9781403942463',
+    language: 'English',
+    programmingLanguage: null,
+    onlineContent: true,
+    thirdParty: {
+      goodreads: {
+        rating: 4.35,
+        ratingsCount: 370,
+        reviewsCount: 18,
+        fiveStarRatingCount: 211,
+        oneStarRatingCount: 6,
+      },
+    },
+    highlighted: true,
+  },
+  {
+    title: 'The Personal MBA: Master the Art of Business',
+    author: 'Josh Kaufman',
+    publisher: 'Portfolio',
+    publicationDate: '2010-12-30',
+    keywords: ['business'],
+    pages: 416,
+    format: 'hardcover',
+    ISBN: '9781591843528',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.11,
+        ratingsCount: 40119,
+        reviewsCount: 1351,
+        fiveStarRatingCount: 18033,
+        oneStarRatingCount: 1090,
+      },
+    },
+  },
+  {
+    title: 'Crafting Interpreters',
+    author: 'Robert Nystrom',
+    publisher: 'Genever Benning',
+    publicationDate: '2021-07-28',
+    keywords: [
+      'computer science',
+      'compilers',
+      'engineering',
+      'interpreters',
+      'software',
+      'engineering',
+    ],
+    pages: 865,
+    format: 'paperback',
+    ISBN: '9780990582939',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.7,
+        ratingsCount: 253,
+        reviewsCount: 23,
+        fiveStarRatingCount: 193,
+        oneStarRatingCount: 0,
+      },
+    },
+  },
+  {
+    title: 'Deep Work: Rules for Focused Success in a Distracted World',
+    author: 'Cal Newport',
+    publisher: 'Grand Central Publishing',
+    publicationDate: '2016-01-05',
+    edition: 1,
+    keywords: ['work', 'focus', 'personal development', 'business'],
+    pages: 296,
+    format: 'hardcover',
+    ISBN: '9781455586691',
+    language: 'English',
+    thirdParty: {
+      goodreads: {
+        rating: 4.19,
+        ratingsCount: 144584,
+        reviewsCount: 11598,
+        fiveStarRatingCount: 63405,
+        oneStarRatingCount: 1808,
+      },
+    },
+    highlighted: true,
+  },
+];
+
+const [firstBook, secondBook] = books;
+const [, , thirdBook] = books;
+console.log(firstBook, secondBook, thirdBook);
+
+const ratings = [
+  ['rating', 4.19],
+  ['ratingsCount', 144584],
+];
+const [[, rating], [, ratingsCount]] = ratings;
+console.log(rating, ratingsCount);
+
+const ratingStars = [63405, 1808];
+const [fiveStarRatings, oneStarRatings, threeStarRatings = 0] = ratingStars;
+
+// DESTRUCTURING OBJECTS
+const { title, author, ISBN } = firstBook;
+const { keywords: tags } = firstBook;
+
+const { language, programmingLanguage = 'unknown' } = books[6];
+
+let bookTitle = 'unknown';
+let bookAuthor = 'unknown';
+
+({ title: bookTitle, author: bookAuthor } = firstBook);
+
+const {
+  thirdParty: {
+    goodreads: { rating: bookRating },
+  },
+} = firstBook;
+console.log(bookRating);
+
+function printBookInfo({ title, author, year = 2002 }) {
+  console.log(`${title} by ${author}, ${year}`);
+}
+
+printBookInfo({
+  title: 'Algorithms',
+  author: 'Robert Sedgewick',
+  year: '2011',
+});
+
+printBookInfo({ title: 'Algorithms', author: 'Robert Sedgewick' });
+
+// SPREAD OPERATOR
+const bookAuthors = [...firstBook.author, ...secondBook.author];
+console.log(bookAuthors);
+
+function spellWord(str) {
+  console.log(...str);
+}
+spellWord('javascript');
+
+// REST PATTERN AND PARAMETERS
+const [mainKeyWord, ...rest] = firstBook.keywords;
+console.log(mainKeyWord, rest);
+
+const { publisher: bookPublisher, ...restOfTheBook } = secondBook;
+
+function printBookAuthorsCount(title, ...authors) {
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+
+// SHORT CIRCUITING
+function hasExamplesInJava(book) {
+  console.log(book.programmingLanguage === 'Java' || 'no data available');
+}
+
+hasExamplesInJava(books[0]);
+hasExamplesInJava(books[1]);
+
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent &&
+    console.log(`${books[i].title} provides online content`);
+}
+
+// NULLISH COALESCING OPERATOR
+for (let i = 0; i < books.length; i++) {
+  books[i].onlineContent ??
+    console.log(`${books[i].title} provides no data about its online content`);
+}
+
+// LOGICAL ASSIGNMENT OPERATORS
+for (let i = 0; i < books.length; i++) {
+  books[i].edition ||= 1;
+}
+
+for (let i = 0; i < books.length; i++) {
+  books[i].thirdParty.goodreads.rating &&= !(
+    books[i].thirdParty.goodreads.rating < 4.2
+  );
+}
+
+// LOOPING ARRAYS
+let pageSum = 0;
+for (const book of books) pageSum += book.pages;
+
+console.log(pageSum);
+
+const allAuthors = [];
+for (const book of books) {
+  if (typeof book.author === 'string') allAuthors.push(book.author);
+  else {
+    for (const author of book.author) {
+      allAuthors.push(author);
+    }
+  }
+}
+console.log(allAuthors);
+
+for (const [i, author] of allAuthors.entries()) {
+  console.log(`${i + 1}: ${author}`);
+}
+
+// OBJECT LITERALS
+const bookData = [
+  ['title', 'Computer Networking: A Top-Down Approach'],
+  ['author', ['James F. Kurose', 'Keith W. Ross']],
+  ['publisher', 'Addison Wesley'],
+];
+
+// Do the rest
+const newBook = {
+  [bookData[0][0]]: bookData[0][1],
+  [bookData[1][0]]: bookData[1][1],
+  [bookData[2][0]]: bookData[2][1],
+};
+
+console.log(newBook);
+
+const pages = 880;
+
+const newBook2 = {
+  title: 'The C Programming Language',
+  author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
+  pages,
+};
+
+// OPTIONAL CHAINING
+function getFirstKeyword(book) {
+  return book.keywords?.[1];
+}
+
+console.log(getFirstKeyword(books[0]));
+console.log(getFirstKeyword(newBook2));
+
+// LOOPING OBJECTS: OBJECT KEYS, VALUES, ENTRIES
+const entries = [];
+for (const key of Object.keys(firstBook.thirdParty.goodreads)) {
+  entries.push([key]);
+}
+console.log(entries);
+
+for (const [i, el] of Object.values(firstBook.thirdParty.goodreads).entries()) {
+  entries[i].push(el);
+}
+console.log(entries);
+
+const entries2 = Object.entries(firstBook.thirdParty.goodreads);
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,29 +1,288 @@
 'use strict';
 
+const arr = [1, 2, 3];
+const [x, y, z] = arr;
+
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '10:00',
+    address = 'Default Address',
+  }) {
+    // console.log(obj);
+    console.log(
+      `Order recieved: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  //es6
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your pasta with ingredients: ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+  //es6
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+  //es6 enhanced object literals
+  openingHours,
 };
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+const [starter, mainCourse] = restaurant.order(1, 2);
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 7]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default Values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+
+// OBJECTS
+const { name, openingHours: openinghours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested Objects
+const {
+  fri: { open, close },
+} = openingHours;
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: '22 Blecker St',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+// Spread Operator
+const array = [7, 8, 9];
+const badNewArr = [1, 2, ...array];
+console.log(badNewArr);
+console.log(...badNewArr);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join two arrays
+const allMenu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+console.log(allMenu);
+
+// Iterables - strings, map, arrays, sets | not objects
+const str = 'Mayank';
+const letters = [...str, ' ', 'U.'];
+console.log(letters);
+
+// const ingredients = [
+//   prompt("Let's make Pasta!: Ingredient 1"),
+//   prompt("Let's make Pasta!: Ingredient 2"),
+//   prompt("Let's make Pasta!: Ingredient 3"),
+// ];
+
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]);
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurant = { ...restaurant, founder: 'Tom', foundedIn: '1925' };
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Risoto Roma';
+console.log(restaurantCopy.name);
+console.log(restaurant.name);
+
+// Rest Pattern
+// Spread used on right side of = | Rest used on left side of =
+const [first, second, ...others] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(first, second, others);
+
+const [pizza, , risotto, ...otherFoods] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFoods);
+
+// Objects
+const { sat, ...weekday } = restaurant.openingHours;
+console.log(sat, weekday);
+
+// Functions
+const add = function (...numbers) {
+  // console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+  return sum;
+};
+
+add(2, 3);
+add(1, 2, 3, 4, 5);
+add(1);
+
+const xx = [23, 1, 95];
+add(...xx);
+
+restaurant.orderPizza('mushroom', 'onion', 'olives', 'spinach');
+
+// Shortcircuiting
+console.log('-----OR-----');
+console.log(3 || 'Mayank');
+console.log('' || 'Mayank');
+console.log(true || 'Mayank');
+console.log(undefined || 'Mayank');
+console.log(undefined || null);
+console.log(undefined || null || false);
+console.log(undefined || 0 || '' || ' ' || 'hello' || 23 || null);
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+const guests2 = restaurant.numGuests || 10;
+console.log(guests1, guests2);
+
+console.log('-----AND------');
+console.log(0 && 'Mayank');
+console.log(7 && 'Mayank');
+
+console.log('Hello' && 23 && null && 'mayank');
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushroom', 'spinach');
+}
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+// NULLISH OPERATOR : null and undefined
+const guests4 = restaurant.numGuests ?? 10;
+
+const rest1 = { name: 'Caapri', numGuests: 10 };
+const rest2 = { name: 'Popa', numGuests: 2 };
+const rest3 = { name: 'Caapri', owner: 'Giovani' };
+
+rest1.numGuests = rest1.numGuests || 20;
+rest2.numGuests = rest2.numGuests || 10;
+
+// OR ASSIGNMENT OPERATOR
+rest1.numGuests ||= 10;
+rest2.numGuests ||= 10;
+
+console.log(rest1, rest2);
+
+// FOR-OF LOOP
+const totalMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of totalMenu) console.log(item);
+for (const [i, el] of totalMenu.entries()) console.log(`${i + 1}: ${el}`);
+
+console.log(openinghours);
+
+// without optional chaining
+if (restaurant.openingHours && restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// OPTIONAL CHAINING
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// METHODS
+console.log(restaurant.order?.(0, 1) ?? 'method does not exist');
+console.log(restaurant.risoto?.(0, 1) ?? 'method does not exist');
+
+// ARRAYS
+// const users = [{ name: 'mayank', email: 'mayank@gmail.com' }];
+const users = [];
+console.log(users[0]?.name ?? 'User array empty');
+
+// LOOPING OBJECTS: KEYS, VALUES, ENTRIES
+const properties = Object.keys(openinghours);
+for (const day of Object.keys(openingHours)) console.log(day);
+
+// console.log(`We are open on`)
+console.log(`We are open for ${properties.length} days.`);
+
+let openStr = '';
+for (const day of properties) {
+  openStr += `${day}, `;
+}
+console.log(openStr);
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries)
+  console.log(`On ${key}, we open at ${open} and close at ${close}`);
