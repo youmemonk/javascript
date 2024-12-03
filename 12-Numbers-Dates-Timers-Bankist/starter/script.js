d4f635e5215a5d727c3707b520c52ac5899ce5e1
mayank <ukeym160@gmail.com>
2024-12-03 13:56:24 +0530

    Math and rounding

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -251,3 +251,72 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// console.log(0.1 + 0.2);
+// console.log(3 / 10);
+
+// // Convert string to number
+// console.log(Number('23'));
+// console.log(+'23');
+
+// // Parsing
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e23', 10));
+// console.log(Number.parseFloat('2.5rem'));
+// console.log(Number.parseInt('2.5rem'));
+// console.log(parseInt('2.5rem'));
+
+// // isNaN
+// console.log(Number.isNaN('20'));
+// console.log(Number.isNaN(+'20x'));
+// console.log(Number.isNaN(10 / 0)); // infinity
+
+// // isFinite - better to use this than isNaN
+// console.log(Number.isFinite(10 / 10));
+// console.log(Number.isFinite(10 / 0));
+
+// Math and rounding
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3));
+console.log(Math.max(2, 3, 5, 6, 7, 12, 10));
+console.log(Math.min(2, 3, 5, 6, 7, 12, 10));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+console.log(Math.random());
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.trunc(Math.random() * (max - min) + 1) + min;
+
+console.log(randomInt(10, 20));
+
+// Rounding
+console.log(Math.trunc(23.3));
+
+// round to nearest integer
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+// round up
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+// round down
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(-5.4));
+console.log(Math.trunc(-5.9));
+
+console.log(Math.floor(-5.4));
+console.log(Math.floor(-5.9));
+// floor is better than trunc
+
+// Rounding decimels
+// toFixed returns a string
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(1));
+console.log((2.7).toFixed(2));
+console.log((2.7).toFixed(4));
